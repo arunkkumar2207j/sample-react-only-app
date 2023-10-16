d7b1e3e6294b2c0acde2c339847bcc95a66684c8
Arun Kamble Personal <arunkamble2207j@gmail.com>
2023-10-16 06:51:11 +0530

    changed content in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React testing from Arun Kamble
         </a>
       </header>
     </div>
